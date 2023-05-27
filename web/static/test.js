c5ddb2344d68642cec4c192daf7a4bd9f2fede06
11Alone11 <anton.stasyuk.0@mail.ru>
2023-05-27 13:13:25 +0300

    test8

--- web/static/test.js
+++ web/static/test.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("load", () => {
   const swiper = new Swiper(".swiper", {
     direction: "vertical",
     mousewheel: {
